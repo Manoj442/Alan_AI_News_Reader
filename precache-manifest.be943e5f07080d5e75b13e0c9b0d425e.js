9cf283d99ec66f80cc07e050b4d7ee8c312504af
Manoj <manoj.gudivada1@gmail.com>
2020-08-07 15:58:04 +0530

    Updates

---
+++ precache-manifest.be943e5f07080d5e75b13e0c9b0d425e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7bc380ae10f8acf85568c4af280db2",
+    "url": "/alan-ai-news-reader/index.html"
+  },
+  {
+    "revision": "7a3747f06aa299a1f3cf",
+    "url": "/alan-ai-news-reader/static/js/2.f886e085.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan-ai-news-reader/static/js/2.f886e085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019a8c90af7029f02aab",
+    "url": "/alan-ai-news-reader/static/js/main.909f751d.chunk.js"
+  },
+  {
+    "revision": "9a072450f8a5dffd77ae",
+    "url": "/alan-ai-news-reader/static/js/runtime-main.d9e626f9.js"
+  },
+  {
+    "revision": "3ad53d6df72fa2870a248ba2f07d3de9",
+    "url": "/alan-ai-news-reader/static/media/AlanAI.3ad53d6d.jpg"
+  },
+  {
+    "revision": "f63fc6d0e3bc0eb64d9d85efc8050f9a",
+    "url": "/alan-ai-news-reader/static/media/News.f63fc6d0.jpg"
+  }
+]);
